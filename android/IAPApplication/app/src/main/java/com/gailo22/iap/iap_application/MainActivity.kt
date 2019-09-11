c8ef241012d0271e9123e62dc86e8f192ba4354c
Montree Bungnasang <montree.bun@truedigital.com>
2019-09-11 14:48:39 +0700

    poc firebase storage trigger

--- android/IAPApplication/app/src/main/java/com/gailo22/iap/iap_application/MainActivity.kt
+++ android/IAPApplication/app/src/main/java/com/gailo22/iap/iap_application/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
@@ -12,16 +14,31 @@ class MainActivity : AppCompatActivity() {
         private val TAG = "MainActivity"
     }
 
-    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button1).setOnClickListener { _ -> addUser() };
+        val db = FirebaseFirestore.getInstance()
+
+        // Manually configure Firebase Options
+        val options = FirebaseOptions.Builder()
+            .setProjectId("sample-firestore-25b7d")
+            .setApplicationId("1:185200102540:android:481b6b4877a4a554")
+            .setApiKey("AIzaSyBkgTWl0ClcqXeyzGZ_jewI2ORhrFj0aaY")
+            .setDatabaseUrl("https://sample-firestore-25b7d.firebaseio.com")
+            .build()
+
+        FirebaseApp.initializeApp(this, options, "app2")
+        val app2 = FirebaseApp.getInstance("app2")
+
+        val db2 = FirebaseFirestore.getInstance(app2)
+
+        findViewById<Button>(R.id.button1).setOnClickListener { _ -> addUser(db) };
+        findViewById<Button>(R.id.button2).setOnClickListener { _ -> addUser(db2) };
     }
 
-    fun addUser() {
+    fun addUser(db: FirebaseFirestore) {
         // Create a new user with a first and last name
         val user = hashMapOf(
             "first" to "Ada android",
--- firebase/simple-firebase-app/functions/index.js
+++ firebase/simple-firebase-app/functions/index.js
@@ -1,10 +1,31 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+const firebase = require("firebase/app");
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+// const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
+var db = admin.firestore();
+
+// const bucket = admin.storage().bucket();
+
+// var storage = firebase.storage();
+// var storageRef = storage.ref();
+// var receiptRef = storageRef.child('receipt');
+
+// Get a non-default Storage bucket
+// var storage = firebase.app().storage("gs://my-custom-bucket");
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
@@ -57,3 +78,71 @@ exports.updateUser = functions.firestore
       console.log("newValue: " + newValue);
 
     });
+
+exports.concurrentUpdate = functions
+    .region('us-central1')
+    .firestore
+    .document('concurrency/{mId}/{makeId}/{randId}')
+    .onUpdate(async(change, context) => {
+    await db.doc('concurrency/configuration').get().then(doc => {
+     if (!doc.exists) {
+      console.log('No such document!');
+     } else {
+      db.collection('concurrency').doc(context.params.mId).collection(context.params.makeId)
+       .get()
+       .then(docSet => {
+        let ccu = docSet.size;
+        let counter = 1;
+        let loc = Math.floor(new Date().getTime()/1000);
+        let myDev = [];
+        let myAgent = [];
+        docSet.forEach(docu => {
+         let serv = Math.floor(docu.data().timestamp.toMillis()/1000);
+         if(loc - serv > doc.data().del_time) {
+          db.doc('concurrency/'+context.params.mId+'/'+context.params.makeId+'/'+docu.id).delete();
+          ccu--;
+         }else if(counter <= doc.data().maximum_device){
+           myDev.push(docu.id);
+           myAgent.push("|"+docu.id+"|"+docu.data().os+"|"+docu.data().nick_name+"|"+docu.data().device_model+"|");
+           counter++;
+         }
+        });
+        if(ccu >= doc.data().maximum_device){
+           let myData = {"ssoid": context.params.makeId,"utc": loc,"ttl": doc.data().rd_ttl,"device": myDev,"body": myAgent};
+           console.log('JSON',myData);
+        }
+       });
+     }
+    }).catch(err => {
+      console.log('Error getting document', err);
+    });
+  });
+
+  exports.receiptCreated = functions.storage.object().onFinalize(async (object) => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+    const metageneration = object.metageneration;
+    const fileName = path.basename(filePath);
+    const bucket = storage.bucket(object.bucket);
+
+    console.log("fileBucket: " + fileBucket);
+    console.log("filePath: " + filePath);
+    console.log("contentType: " + contentType);
+    console.log("metageneration: " + metageneration);
+    console.log("fileName: " + fileName);
+
+    if (!fileName.endsWith('.json')) {
+      return console.log('Not a json file');
+    }
+
+    bucket.file(filePath).download((err, contents) => {
+      if (err) {
+          console.log('error', err);
+          return null
+      }
+
+      console.log(contents.toString());
+    });
+
+  });
