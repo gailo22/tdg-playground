b2c26eb5920b66b063b11cfe11f0720b9419ea79
gailo22 <gailo22@gmail.com>
2019-10-17 18:01:16 +0700

    add go-practices

---
+++ go/go-practices/go.mod
@@ -0,0 +1,5 @@
+module go-practices/hello
+
+go 1.12
+
+require rsc.io/quote v1.5.2
---
+++ go/go-practices/hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"go-practices/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+}
