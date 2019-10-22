e80ae7ff61cb2fb5737f309205abfd317d402678
gailo22 <gailo22@gmail.com>
2019-10-22 17:26:34 +0700

    update

--- go/go-practices/go.mod
+++ go/go-practices/go.mod
@@ -2,4 +2,7 @@ module go-practices/v1
 
 go 1.12
 
-require rsc.io/quote/v3 v3.1.0
+require (
+	github.com/gorilla/mux v1.7.3
+	rsc.io/quote/v3 v3.1.0
+)
---
+++ go/go-practices/restapi/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Book struct {
+	ID     string  `json: "id"`
+	Isbn   string  `json: "isbn"`
+	Title  string  `json: "title"`
+	Author *Author `json: "author"`
+}
+
+type Author struct {
+	Firstname string `json: "firstname"`
+	Lastname  string `json: "lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func udpateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	books := append(books, Book{ID: "1", Isbn: "233324", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", udpateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
