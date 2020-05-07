8fc3fc815857bd7acb944111b191ffe3507a0ce2
Montree Bungnasang <montree.bun@truedigital.com>
2020-05-07 23:28:45 +0700

    add application classes

--- flutter/webrtc_app_2/lib/injection.iconfig.dart
+++ flutter/webrtc_app_2/lib/injection.iconfig.dart
@@ -4,6 +4,12 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:webrtc_app_2/application/auth/auth_bloc.dart';
+import 'package:webrtc_app_2/domain/auth/i_auth_facade.dart';
+import 'package:webrtc_app_2/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:get_it/get_it.dart';
 
-void $initGetIt(GetIt g, {String environment}) {}
+void $initGetIt(GetIt g, {String environment}) {
+  g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
+  g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
+}
