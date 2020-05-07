285c1ef74fe37229f9eaaa6f45b11f2f4e70a925
Montree Bungnasang <montree.bun@truedigital.com>
2020-05-08 00:08:01 +0700

    fix build error

--- flutter/webrtc_app_2/lib/injection.iconfig.dart
+++ flutter/webrtc_app_2/lib/injection.iconfig.dart
@@ -4,17 +4,25 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:webrtc_app_2/infrastructure/core/firebase_injectable_module.dart';
 import 'package:webrtc_app_2/infrastructure/auth/firebase_user_mapper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:webrtc_app_2/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:webrtc_app_2/domain/auth/i_auth_facade.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:webrtc_app_2/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:webrtc_app_2/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  g.registerLazySingleton<FirebaseAuth>(
+      () => firebaseInjectableModule.firebaseAuth);
   g.registerLazySingleton<FirebaseUserMapper>(() => FirebaseUserMapper());
+  g.registerLazySingleton<Firestore>(() => firebaseInjectableModule.firestore);
+  g.registerLazySingleton<GoogleSignIn>(
+      () => firebaseInjectableModule.googleSignIn);
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
   g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
 
@@ -27,3 +35,5 @@ void $initGetIt(GetIt g, {String environment}) {
         ));
   }
 }
+
+class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
