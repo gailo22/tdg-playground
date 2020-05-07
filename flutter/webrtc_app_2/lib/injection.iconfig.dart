bfb14882ea40d284c8b9748da8ee26ef4987ca42
Montree Bungnasang <montree.bun@truedigital.com>
2020-05-07 23:33:01 +0700

    add infrastructure classes

--- flutter/webrtc_app_2/lib/injection.iconfig.dart
+++ flutter/webrtc_app_2/lib/injection.iconfig.dart
@@ -4,12 +4,26 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:webrtc_app_2/application/auth/auth_bloc.dart';
+import 'package:webrtc_app_2/infrastructure/auth/firebase_user_mapper.dart';
+import 'package:webrtc_app_2/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:webrtc_app_2/domain/auth/i_auth_facade.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:webrtc_app_2/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:webrtc_app_2/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
-  g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
+  g.registerLazySingleton<FirebaseUserMapper>(() => FirebaseUserMapper());
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
+  g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
+
+  //Register prod Dependencies --------
+  if (environment == 'prod') {
+    g.registerLazySingleton<IAuthFacade>(() => FirebaseAuthFacade(
+          g<FirebaseAuth>(),
+          g<GoogleSignIn>(),
+          g<FirebaseUserMapper>(),
+        ));
+  }
 }
