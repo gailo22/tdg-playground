6e2f9490b95b2026d7dcc2cd403beffb56546f1e
Montree Bungnasang <montree.bun@truedigital.com>
2020-05-08 00:11:07 +0700

    add sample android app

---
+++ android/FCM/TMNApplication/app/src/main/java/com/gailo22/tmnapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gailo22.tmnapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
