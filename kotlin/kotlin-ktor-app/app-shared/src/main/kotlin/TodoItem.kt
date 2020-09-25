5c50eaf5dc026efa8f54131624050360fa319496
Montree Bungnasang <montree.bun@truedigital.com>
2020-09-25 16:33:06 +0700

    update project structure

---
+++ kotlin/kotlin-ktor-app/app-shared/src/main/kotlin/TodoItem.kt
@@ -0,0 +1,22 @@
+package com.gailo22.app.shared
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
+import java.time.LocalDate
+
+data class TodoItem(
+    val title: String,
+    val details: String,
+    val assignedTo: String,
+    @JsonSerialize(using = ToStringSerializer::class)
+    @JsonDeserialize(using = LocalDateDeserializer::class)
+    val dueDate: LocalDate,
+    val importance: Importance
+)
+
+enum class Importance {
+    LOW, MEDIUM, HIGH
+}
+
---
+++ kotlin/kotlin-ktor-app/settings.gradle.kts
@@ -0,0 +1,5 @@
+
+rootProject.name = "kotlin-ktor-app"
+
+include("app-shared")
+include("app-restapi")
