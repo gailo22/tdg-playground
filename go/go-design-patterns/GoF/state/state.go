9ac8e06616dc9982228b37a02cc780743de5951b
gailo22 <gailo22@gmail.com>
2021-01-11 09:53:51 +0700

    state pattern

---
+++ go/go-design-patterns/GoF/state/state.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Switch struct {
+	State State
+}
+
+func NewSwitch() *Switch {
+	return &Switch{NewOffState()}
+}
+
+func (s *Switch) On() {
+	s.State.On(s)
+}
+
+func (s *Switch) Off() {
+	s.State.Off(s)
+}
+
+type State interface {
+	On(sw *Switch)
+	Off(sw *Switch)
+}
+
+type BaseState struct{}
+
+func (b *BaseState) On(sw *Switch) {
+	fmt.Println("Light is already on")
+}
+
+func (b *BaseState) Off(sw *Switch) {
+	fmt.Println("Light is already off")
+}
+
+type OnState struct {
+	BaseState
+}
+
+func NewOnState() *OnState {
+	fmt.Println("Light turn on")
+	return &OnState{BaseState{}}
+}
+
+func (o *OnState) Off(sw *Switch) {
+	fmt.Println("Turning the light off...")
+	sw.State = NewOffState()
+}
+
+type OffState struct {
+	BaseState
+}
+
+func NewOffState() *OffState {
+	fmt.Println("Light turn off")
+	return &OffState{BaseState{}}
+}
+
+func (o *OffState) On(sw *Switch) {
+	fmt.Println("Turning the light on...")
+	sw.State = NewOnState()
+}
+
+func main() {
+	sw := NewSwitch()
+	sw.On()
+	sw.Off()
+	sw.Off()
+}
