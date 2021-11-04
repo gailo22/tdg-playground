d262569217f1f5b747d276d1f415867e0c85b8fc
gailo22 <gailo22@gmail.com>
2021-11-04 12:59:06 +0700

    update

--- go/go-practices/goroutine1/customer/customer.go
+++ go/go-practices/goroutine1/customer/customer.go
@@ -1,56 +1,56 @@
-package customer
+package 
 
 import "fmt"
 
-type Customer struct {
+type  struct {
 	ID   int
 	Name string
 }
 
-func (c Customer) String() string {
+func (c ) String() string {
 	return fmt.Sprintf(
 		"ID:\t%v\n"+
 			"Name:\t%q\n", c.ID, c.Name)
 }
 
 var Customers = []Customer{
-	Customer{
+	{
 		ID:   1,
 		Name: "John",
 	},
-	Customer{
+	{
 		ID:   2,
 		Name: "Jane",
 	},
-	Customer{
+	{
 		ID:   3,
 		Name: "Mark",
 	},
-	Customer{
+	{
 		ID:   4,
 		Name: "Richard",
 	},
-	Customer{
+	{
 		ID:   5,
 		Name: "Anne",
 	},
-	Customer{
+	{
 		ID:   6,
 		Name: "Jin",
 	},
-	Customer{
+	{
 		ID:   7,
 		Name: "Anand",
 	},
-	Customer{
+	{
 		ID:   8,
 		Name: "Suzy",
 	},
-	Customer{
+	{
 		ID:   9,
 		Name: "Lydia",
 	},
-	Customer{
+	{
 		ID:   10,
 		Name: "Scarlet",
 	},
